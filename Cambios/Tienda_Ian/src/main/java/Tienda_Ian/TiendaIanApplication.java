e41d1ef8405822ee2b0695c49c487108a18f9666
IanBrenes <brenes.ian.cr7@gmail.com>
2026-01-22 19:50:16 -0600

    Commit Tarea Semana 2

---
+++ Cambios/Tienda_Ian/src/main/java/Tienda_Ian/TiendaIanApplication.java
@@ -0,0 +1,13 @@
+package Tienda_Ian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIanApplication.class, args);
+	}
+
+}
---
+++ Cambios/Tienda_Ian/src/test/java/Tienda_Ian/TiendaIanApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Ian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
